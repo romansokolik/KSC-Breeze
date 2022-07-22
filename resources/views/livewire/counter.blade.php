d60c8f22c33d2bf422c22b17089052b97f5b99bb
Roman Sokolik <roman@sokolik.ca>
2022-07-21 23:00:48 -0400

    Initial Commit

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center">
+    {{-- The best athlete wants his opponent at his best. --}}
+    <h1>Hello World!</h1>
+    <button type="button" class="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500" wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
